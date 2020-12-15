97c068c10c9032e455519b4682de3e2c8760fd2a
Wisinewski <g_gomees@hotmail.com>
2020-12-15 17:40:52 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
   mongodbMemoryServerOptions: {
-    instance: {
-      dbName: 'jest'
-    },
     binary: {
-      version: '4.0.3',
+      version: '4.4.1',
       skipMD5: true
     },
-    autoStart: false
+    autoStart: false,
+    instance: {}
   }
 }
