0833a3594196f5f074a228f2119ee537f73e8c62
Wisinewski <g_gomees@hotmail.com>
2021-01-03 00:03:24 -0300

    feat: add LoginValidationFactory

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,11 +1,11 @@
 import { makeSignUpValidation } from './signup-validation'
-import { LogControllerDecorator } from './../decorators/log'
-import { Controller } from './../../presentation/protocols/controller'
-import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account'
-import { LogMongoRepository } from './../../infra/db/mongodb/log-repository/log'
-import { BcryptAdapter } from './../../infra/criptography/bcrypt-adapter'
-import { DbAddAccount } from './../../data/usecases/add-account/db-add-account'
-import { SignUpController } from './../../presentation/controllers/signup/signup'
+import { LogControllerDecorator } from '../../decorators/log'
+import { Controller } from '../../../presentation/protocols/controller'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
