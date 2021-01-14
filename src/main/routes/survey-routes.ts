f7c31e77b033aec4292761c6edd453b3385248cb
Wisinewski <g_gomees@hotmail.com>
2021-01-14 15:15:57 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.ts
@@ -0,0 +1,7 @@
+import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.post('/surveys', adaptRoute(makeAddSurveyController()))
+}
